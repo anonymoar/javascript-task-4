347d7c65583707c24d25283e0606fb5c670fd813
anonymoar <anonymoar@yandex.ru>
2019-01-06 16:43:06 +0500

    Решила задачку

--- emitter.js
+++ emitter.js
@@ -4,13 +4,15 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    const events = {};
+
     return {
 
         /**
@@ -18,26 +20,63 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!(event in events)) {
+                events[event] = [];
+            }
+
+            events[event].push({ context, handler });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            const eventRegExp = new RegExp(`^${event}[.]`);
+
+            for (const eventName of Object.keys(events)) {
+                if (eventName === event || eventRegExp.test(eventName)) {
+                    events[eventName] = events[eventName].filter(
+                        descriptor => descriptor.context !== context
+                    );
+                }
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            const eventNames = [];
+            const splittedEvent = event.split('.');
+
+            for (let i = splittedEvent.length - 1; i > 0; i--) {
+                eventNames.push(splittedEvent.slice(0, i + 1).join('.'));
+            }
+
+            eventNames.push(splittedEvent[0]);
+
+            for (const eventName of eventNames) {
+                if (!(eventName in events)) {
+                    continue;
+                }
+                for (const descriptor of events[eventName]) {
+                    descriptor.handler.call(descriptor.context);
+                }
+            }
+
+            return this;
         },
 
         /**
